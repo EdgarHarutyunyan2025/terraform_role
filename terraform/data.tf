2126a359e06a5a05b5204fb01ff21b27e3470dbf
edgar_harutyunyan_2025 <edgar_harutyunyan_2025@mail.ru>
2025-04-01 14:29:00 +0400

    test

--- terraform/data.tf
+++ terraform/data.tf
@@ -1,3 +1,21 @@
+terraform {
+  backend "s3" {
+    bucket = "mygithub-action-test1"
+    key    = "prod/green-blue"
+    region = "eu-central-1"
+  }
+}
+
+#data "terraform_remote_state" "green-blue" {
+#  backend = "s3"
+#
+#  config = {
+#    bucket = "mygithub-action-test1"
+#    key    = "prod/green-blue"
+#    region = "eu-central-1"
+#  }
+#}
+
 data "aws_availability_zones" "working" {}
 
 data "aws_ami" "amazon_linux_latest" {
